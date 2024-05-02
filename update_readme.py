10324c1a4a583c8507f2fb19630d6930f4b1bdaf
sertemo <tejedor.moreno@gmail.com>
2024-05-02 16:14:26 +0200

    añadido un workflow separado para el update del readme

--- update_readme.py
+++ update_readme.py
@@ -7,18 +7,23 @@ def update_readme():
     # Leer la configuración del proyecto
     with open("pyproject.toml", "r") as file:
         data = toml.load(file)
-        project_version = data["tool"]["poetry"]["version"]
+        current_version = data["tool"]["poetry"]["version"]
 
-    # Leer el contenido actual de README.md y actualizar
+    # Leer el contenido actual de README.md y actualizar si es necesario
     with open("README.md", "r") as file:
         readme_contents = file.readlines()
 
-    # La versión está en la segunda fila
-    readme_contents[1] = f"### v{project_version}\n"
+    new_readme_content = f"### v{current_version}\n"
 
-    # Escribir el contenido actualizado de nuevo a README.md
-    with open("README.md", "w") as file:
-        file.writelines(readme_contents)
+    if readme_contents[1] != new_readme_content:
+        # La versión está en la segunda fila
+        readme_contents[1] = new_readme_content
+        # Escribir el contenido actualizado de nuevo a README.md
+        with open("README.md", "w") as file:
+            file.writelines(readme_contents)
+        print("README updated")
+    else:
+        print("No update needed")
 
 
 if __name__ == "__main__":
